1c9bfb8745650d19c08d22a2bb7bc359ac828bfe
Diego Sanchez <diegosmtattoo@gmail.com>
2024-06-18 20:18:12 -0300

    commit final

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -44,7 +44,7 @@ import {reactive } from "vue";
 
 <template>
     <div class="container">
-    <header class="p-4 mb-4 mt-4 bg-light rounded-4">
+    <header class="p-4 mb-4 mt-4 rounded-4">
         <h1>Calculadora Aritmética</h1>
     </header>
     <div class="calculadora">
@@ -65,7 +65,7 @@ import {reactive } from "vue";
         <div class="col-md-2">
             <input class="form-control"  v-model="estado.b" @keyup="calculaResultado()" @change="calculaResultado()" type="number" placeholder="insira um número">
         </div>
-        <span class="col-md-3  bg-light rounded-4 resultado"> = {{ estado.resultado }}</span>
+        <span class="col-md-4 rounded-4 resultado"> {{ estado.resultado }}</span>
         </div>      
         </form>
 
@@ -74,11 +74,27 @@ import {reactive } from "vue";
 </template>
 
 <style scoped>
+.container{
+    max-width: 700px;
+}
+header{
+    background-color: rgb(236, 236, 236);
+}
 input, span{
     display: inline;
+    font-size: 24px;
+    padding-top: 4px;
+}
+select{
+    margin-top: 8px;
+    background-color:  rgb(236, 236, 236);
+    border: 0cap;
 }
 .resultado{
-    font-size: 27px;
-    margin-top: -5px;
+    padding-top: 4px;
+    padding-right: 16px;
+    background-color: rgb(236, 236, 236);
+    text-align: end;
 }
+
 </style>
