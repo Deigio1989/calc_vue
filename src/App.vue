589fb0431596e4e7ad5b6743d9d9de85f305e953
Diego Sanchez <diegosmtattoo@gmail.com>
2024-06-18 15:34:58 -0300

    commit final

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {reactive } from "vue";
+  
+  const estado = reactive({
+    operacao : '+',
+    resultado : 0,
+    a:0,
+    b:0
+
+  })
+
+  const calculaResultado =() =>{
+    switch(estado.operacao){
+      case '+':
+        return soma();
+      case '-':
+        return subtracao(); 
+      case '*':
+        return multiplicacao();
+      case '/':
+        return divisao();      
+    }
+  }
+
+  function soma(){
+    estado.resultado = estado.a + estado.b ;
+  }
+  function subtracao(){
+    estado.resultado = estado.a - estado.b ;
+  }
+  function multiplicacao(){
+    estado.resultado = estado.a * estado.b ;
+  }
+  function divisao(){
+    if (estado.b==0) {
+      estado.resultado = "Divisão por zero";      
+    }
+    else{
+      estado.resultado = estado.a / estado.b ;
+    }
+  }
+    
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <header class="p-4 mb-4 mt-4 bg-light rounded-4">
+      <h1>Calculadora Aritmética</h1>
+    </header>
+    <div class="calculadora">
+      <form >
+        <div class="row">
+          <div class="col-md-2">
+          <input class="form-control" v-model="estado.a" @change="calculaResultado()" type="number" placeholder="insira um número">
+        </div>
+        <div class="col-md-1">
+          <select class="form-control" v-model="estado.operacao" @change="calculaResultado()" >
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        </div>
+        <div class="col-md-2">
+          <input class="form-control"  v-model="estado.b" @change="calculaResultado()" type="number" placeholder="insira um número">
+        </div>
+        <span class="col-md-3  bg-light rounded-4 resultado"> = {{ estado.resultado }}</span>
+        </div>      
+      </form>
+
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+input, span{
+  display: inline;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.resultado{
+  font-size: 27px;
+  margin-top: -5px;
 }
 </style>
